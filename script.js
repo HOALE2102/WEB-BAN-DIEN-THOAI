3321f03cc34550e546ed5e36e40f6b14765964be
baotram05 <nguyenlebaotram55@gmail.com>
2025-12-14 11:55:07 +0700

    baotram hoan thanh tim kiem

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ function renderUserView() {
         container.appendChild(card);
     });
 }
+// hienthi
+let filteredProductsAdmin = [...products]; 
+let currentProductDetailAdmin = null;
+let currentAdminViewMode = 'user'; 
+
+function handleSearch(keyword) {
+    const lowerKeyword = keyword.toLowerCase();
+    filteredProducts = products.filter(p => p.name.toLowerCase().includes(lowerKeyword) || p.brand.toLowerCase().includes(lowerKeyword));
+    
+    // Chỉ render lại user view khi tìm kiếm
+    if (currentViewMode === 'user') renderUserView();
+}
